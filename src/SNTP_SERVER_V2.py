897a5fd33f2f44643d5c493e2241d384139ea62e
Bladimir Pardo <bladimirpardomolina@gmail.com>
2022-11-28 15:24:20 -0300

    v2 listo
    para realizar pruebas y refinar

---
+++ src/SNTP_SERVER_V2.py
@@ -0,0 +1,202 @@
+import threading
+import datetime
+import struct
+import select
+import socket
+import queue
+import math
+import time
+
+# Variables Globales
+taskQueue = queue.Queue()
+stopFlag = False
+
+class PAQUETE_SNTP:
+    
+    _PACKET_FORMAT = "!B B B b 11I"
+    """packet format to pack/unpack"""
+
+    def __init__(self):
+        """Constructor.
+        Parametros:
+        version      -- NTP version
+        mode         -- packet mode (client, server)
+        tx_timestamp -- packet transmit timestamp
+        """
+        self.leap_indicator = 0
+        self.version_number = 0
+        self.mode = 0
+        self.stratum = 0
+        self.poll_interval = 0
+        self.precision = 0
+        self.root_delay = 0
+        self.root_dispersion = 0
+        self.ref_id = 0
+        self.ref_timestamp = 0
+        self.origin_timestamp = 0
+        self.recv_timestamp = 0
+        self.transmit_timestamp = 0
+
+    def to_data(self):
+        """Convierte el paquete SNTP a un buffer que puede ser enviado por un socket.
+        Returns:
+            buffer que respresnta al paquete SNTP
+        Raises:
+            SNTPException -- in case of invalid field
+        """
+        data = ""
+        data += format(self.leap_indicator, '02b')
+        data += format(self.version_number, '03b')
+        data += format(self.mode, '03b')
+        data += format(self.stratum, '08b')
+        data += format(self.poll_interval, '08b')
+        data += format(self.precision, '08b')
+
+        data += format(math.floor(self.root_delay), '16b') + '0'*16 #+ format((self.root_delay%1)*10^16, '')
+        data += format(math.floor(self.root_dispersion), '16b') + '0'*16
+        # cuidado con ref_id al reves
+        data += ''.join(format(ord(i), '04b') for i in self.ref_id)
+        data += format(int(self.ref_timestamp), '32b') + '0'*32
+        data += format(int(self.origin_timestamp), '32b') + '0'*32
+        data += format(int(self.recv_timestamp), '32b') + '0'*32
+        data += format(int(self.transmit_timestamp), '32b') + '0'*32
+    
+        return data
+
+    def from_data(self, data):
+        """Este metodo traduce el paquete SNTP recibido a la clase PAQUETE_SNTP.
+        Parameters:
+            data -- buffer payload
+        Raises:
+            NTPException -- in case of invalid packet format
+        """
+        self.leap_indicator     = int(data[0, 1], 10)
+        self.version_number     = int(data[2, 4], 10)
+        self.mode               = int(data[5, 7], 10)
+        self.stratum            = int(data[8, 15], 10)
+        self.poll_interval      = int(data[16, 23], 10)
+        self.precision          = int(data[24, 31], 10)
+        self.root_delay         = int(data[32, 47], 10) # +0.data[47]
+        self.root_dispersion    = int(data[64, 79], 10) # +0.data[80]
+        self.ref_id             = chr(int(data[96, 99], 10)) + chr(int(data[100, 103], 10)) + chr(int(data[104, 107], 10)) + chr(int(data[108, 111], 10)) + chr(int(data[112, 115], 10)) + chr(int(data[116, 119], 10)) + chr(int(data[120, 123], 10)) + chr(int(data[124, 127], 10))
+        self.ref_timestamp      = int(data[128, 159], 10) #
+        self.origin_timestamp   = int(data[192, 223], 10)
+        self.recv_timestamp     = int(data[256, 287], 10)
+        self.transmit_timestamp = int(data[320, 351], 10)
+
+class Receptor(threading.Thread):
+    def __init__(self, socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+
+    # levanta el servicio de escucha 
+    def run(self):
+        global taskQueue, stopFlag
+        # Me mantengo escuchando hasta que la Flag me lo indique.
+        while (not stopFlag):
+            # select espera a que el objeto este listo, es decir, que todos sus paquetes hayan llegado
+            rlist, wlist, elist = select.select([self.socket],[],[],0)
+            if len(rlist) != 0:
+                print("%d paquetes recibidos".format(rlist))
+                for tempSocket in rlist:
+                    try:
+                        data,addr = tempSocket.recvfrom(1024)
+                        recvTimestamp = time.time()
+                        taskQueue.put((data,addr,recvTimestamp))
+                    except socket.error:
+                        print("Error al recibir solicitud");        
+
+
+class Procesador(threading.Thread):
+    def __init__(self,socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+
+    def run(self):
+        global taskQueue,stopFlag
+        while (not stopFlag):
+            try:
+                data,addr,recvTimestamp = taskQueue.get()
+                PAQUETE_ENTRADA = PAQUETE_SNTP()
+                PAQUETE_ENTRADA.from_data(data)
+                PAQUETE_ENTRADA.recv_timestamp = recvTimestamp
+
+                PAQUETE_SALIDA = PAQUETE_SNTP()
+                PAQUETE_SALIDA.asing_data()
+                PAQUETE_SALIDA.leap_indicator = 0
+                PAQUETE_SALIDA.version_number = PAQUETE_ENTRADA.version_number
+                if (PAQUETE_ENTRADA.mode == 3):
+                    PAQUETE_SALIDA.mode = 4
+                else:
+                    PAQUETE_SALIDA.mode = 2
+                PAQUETE_SALIDA.stratum = 1
+                PAQUETE_SALIDA.poll_interval = PAQUETE_ENTRADA.poll_interval
+                PAQUETE_SALIDA.precision = 0
+                PAQUETE_SALIDA.root_delay = 0
+                PAQUETE_SALIDA.root_dispersion = 0
+                PAQUETE_SALIDA.ref_id = 0
+                PAQUETE_SALIDA.ref_timestamp = PAQUETE_SALIDA.transmit_timestamp = time.time()
+                PAQUETE_SALIDA.origin_timestamp = PAQUETE_ENTRADA.origin_timestamp
+                PAQUETE_SALIDA.recv_timestamp = PAQUETE_ENTRADA.recv_timestamp
+
+                socket.sendto(str.encode(PAQUETE_SALIDA.to_data()),addr)
+                print("Enviado desde %s hacia %d".format(addr[0],addr[1]))
+            except queue.Empty:
+                continue
+
+
+
+# Clase que describe las tablas del RFC 1361
+class SNTP:
+    # 2 bits
+    LI_TABLE ={
+        0: "no warning",
+        1: "last minute has 61 seconds",
+        2: "last minute has 59 seconds)",
+        3: "alarm condition (clock not synchronized)",
+    }
+    
+    # 3 bits
+    MODE_TABLE = {
+        0: "reserved",
+        1: "symmetric active",
+        2: "symmetric passive",
+        3: "client",
+        4: "server",
+        5: "broadcast",
+        6: "reserved for NTP control messages",
+        7: "reserved for private use",
+    }
+
+    STRATUM_TABLE = {
+        0: "unspecified",
+        1: "primary reference",
+    }
+
+def main():
+    # SERVER
+    localIP     = "0.0.0.0"
+    localPort   = 8000
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPServerSocket.bind((localIP, localPort))
+    print("Link Available")
+
+    Prog1 = Receptor(socket)
+    Prog1.start()
+
+    Prog2 = Procesador(socket)
+    Prog2.start()
+
+    while True:
+        try:
+            time.sleep(0.5)
+        except KeyboardInterrupt:
+            print ("Exiting...")
+            stopFlag = True
+            Prog1.join()
+            Prog2.join()
+            socket.close()
+            print ("Exited")
+            break
+
+main()
--- src/server.py
+++ src/server.py
@@ -1,26 +1,18 @@
-from bitarray import bitarray
+import datetime
 import socket
+import queue
 from math import floor
 from time import time 
+
+
+
+# Codigo inspirado en:
+# https://github.com/limifly/ntpserver/blob/master/ntpserver.py
 
 #CONFIG
 SERVER_VERSION = 3
 SERVER_REFERENCE_CLOCK_ID = 0 # REVISAR
 SERVER_TYPE = 'PRIMARY'
-
-localIP     = "127.0.0.1"
-localPort   = 20001
-bufferSize  = 1024
-
-msgFromServer       = "Datagram Acepted"
-bytesToSend         = str.encode(msgFromServer)
-
-# Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
-print("Link Available")
 
 # Listen for incoming datagrams
 while(True):
@@ -81,5 +73,56 @@ while(True):
     reply[192,255] = originateTimestamp
     reply[256,319] = ServerClock
     reply[320,383] = ServerClock
+"""
+class SNTP:
+    TABLE = {
+        LI                  : int(message[0, 1], 2),
+        VN                  : message[2, 4],
+        mode                : int(message[5, 7], 2),
+        stratum             : int(message[8, 15], 2),
+        poll                : message[16, 23],
+        precision           : message[24, 31],
+        rootDelay           : message[32, 63],
+        rootDispersion      : message[64, 95],
+        referenceIdentifier : message[96, 127],
+        referenceTimestamp  : message[128, 191],
+        originateTimestamp  : message[192, 255],
+        receiveTimestamp    : message[256, 319],
+        transmiteTimestamp  : message[320, 383],
+    }
+    
+    MODE_TABLE = {
+        0: "unspecified",
+        1: "symmetric active",
+        2: "symmetric passive",
+        3: "client",
+        4: "server",
+        5: "broadcast",
+        6: "reserved for NTP control messages",
+        7: "reserved for private use",
+    }
 
-    UDPServerSocket.sendto(str.encode(reply), address)
+    STRATUM_TABLE = {
+        0: "unspecified",
+        1: "primary reference",
+    }
+
+    LEAP_TABLE = {
+        0: "no warning",
+        1: "last minute has 61 seconds",
+        2: "last minute has 59 seconds",
+        3: "alarm condition (clock not synchronized)",
+    }
+"""
+
+def main():
+    # SERVER
+    localIP     = "0.0.0.0"
+    localPort   = 8000
+    bufferSize  = 1024
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPServerSocket.bind((localIP, localPort))
+    print("Link Available")
+
+
+main()
